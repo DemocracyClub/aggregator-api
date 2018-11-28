8b81af5ba570a71ff120abbcddf384b87cc2aeb9
chris48s <chris.shaw480@gmail.com>
2018-11-28 12:15:24 +0000

    log when we issue a 5XX response
    this will allow us to capture this in Sentry

---
+++ aggregator/apps/api/middleware.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LoggerMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code >= 500 and "sandbox" not in request.path:
+            logger.error(f"{response.status_code} - {response.content}")
+        return response
