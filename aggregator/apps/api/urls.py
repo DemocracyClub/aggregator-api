5b0ddc59587c0153da5bd6c01e12948527140afa
chris48s <chris.shaw480@gmail.com>
2018-11-30 16:36:12 +0000

    fix deprecation warnings

--- aggregator/apps/api/urls.py
+++ aggregator/apps/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r"^v1/", include("api.v1.urls", namespace="v1")),
+    url(r"^v1/", include(("api.v1.urls", "v1"), namespace="v1")),
     url(r"^$", RedirectView.as_view(url=reverse_lazy("api:v1:docs"), permanent=True)),
 ]
--- aggregator/urls.py
+++ aggregator/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
-    url(r"^api/", include("api.urls", namespace="api")),
+    url(r"^api/", include(("api.urls", "api"), namespace="api")),
     url(r"^$", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
 
