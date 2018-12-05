c0749456c0df5cc7d360b57a83fbfc1b023c2c66
chris48s <chris.shaw480@gmail.com>
2018-12-05 11:24:48 +0000

    read WDIV key from env var

--- aggregator/settings/constants.py
+++ aggregator/settings/constants.py
@@ -1,3 +1,5 @@
+import os
+
 WDIV_BASE_URL = "https://wheredoivote.co.uk/api/beta/"
-WDIV_API_KEY = None
+WDIV_API_KEY = os.environ.get("WDIV_API_KEY", None)
 WCIVF_BASE_URL = "https://whocanivotefor.co.uk/api/"
