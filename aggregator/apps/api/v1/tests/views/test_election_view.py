47a00001868c75e463738f4905dd14afac601bd6
Sym Roe <sym.roe@talusdesign.co.uk>
2020-08-10 15:25:23 +0100

    Remove unneeded f strings

--- aggregator/apps/api/v1/tests/views/test_election_view.py
+++ aggregator/apps/api/v1/tests/views/test_election_view.py
@@ -14,9 +14,7 @@ class ElectionViewTests(TestCase):
         ),
     )
     def test_list_valid(self):
-        response = self.client.get(
-            f"/api/v1/elections/", HTTP_AUTHORIZATION="Token foo"
-        )
+        response = self.client.get("/api/v1/elections/", HTTP_AUTHORIZATION="Token foo")
         expected = load_fixture(
             "elections_endpoint/test_every_election_list", "expected_output"
         )
@@ -26,7 +24,7 @@ class ElectionViewTests(TestCase):
         mock = Mock(return_value={})
         with patch("api.v1.api_client.EEApiClient.get_election_list", mock):
             self.client.get(
-                f"/api/v1/elections/?limit=100&foo=bar", HTTP_AUTHORIZATION="Token foo"
+                "/api/v1/elections/?limit=100&foo=bar", HTTP_AUTHORIZATION="Token foo"
             )
             # this one should pass the GET params down the call stack
             self.assertDictEqual(
@@ -41,7 +39,7 @@ class ElectionViewTests(TestCase):
     )
     def test_single_valid(self):
         response = self.client.get(
-            f"/api/v1/elections/local.cardiff.ely.by.2019-02-21/",
+            "/api/v1/elections/local.cardiff.ely.by.2019-02-21/",
             HTTP_AUTHORIZATION="Token foo",
         )
         expected = load_fixture(
@@ -53,7 +51,7 @@ class ElectionViewTests(TestCase):
         mock = Mock(return_value={})
         with patch("api.v1.api_client.EEApiClient.get_single_election", mock):
             self.client.get(
-                f"/api/v1/elections/local.cardiff.ely.by.2019-02-21/?&foo=bar",
+                "/api/v1/elections/local.cardiff.ely.by.2019-02-21/?&foo=bar",
                 HTTP_AUTHORIZATION="Token foo",
             )
             mock.assert_called_with("local.cardiff.ely.by.2019-02-21")
