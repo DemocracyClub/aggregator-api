2ef0ebe05c3358f08a932bb892fde7076ead7861
Sym Roe <sym.roe@talusdesign.co.uk>
2020-11-18 11:44:37 +0000

    Remove admin site and stop trying to use a database

--- aggregator/settings/base.py
+++ aggregator/settings/base.py
@@ -19,11 +19,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
     "django.contrib.staticfiles",
     "apiblueprint_view",
     "corsheaders",
@@ -70,12 +67,7 @@ WSGI_APPLICATION = "aggregator.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.dummy"}}
 
 
 # Password validation
