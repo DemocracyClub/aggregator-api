df7eaeb24e526163ae650d25e9574ee4c7023b16
Sym Roe <sym.roe@talusdesign.co.uk>
2020-11-29 18:33:55 +0000

    Remove monkey patch of S3Boto3Storage
    It looks like the updated backend had changed to make our patch
    meaningless

--- aggregator/s3_lambda_storage.py
+++ aggregator/s3_lambda_storage.py
@@ -1,11 +1,7 @@
-import mimetypes
-import os
-
 from pipeline.compilers.sass import SASSCompiler
 
 from django.conf import settings
-from django.core.files.base import File
-from storages.backends.s3boto3 import S3Boto3Storage, SpooledTemporaryFile
+from storages.backends.s3boto3 import S3Boto3Storage
 from django.contrib.staticfiles.storage import ManifestFilesMixin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from pipeline.storage import PipelineMixin
@@ -28,58 +24,59 @@ class PatchedS3Boto3Storage(S3Boto3Storage):
     as this may yield unexpected behaviour.
     """
 
-    def _save(self, name, content):
-        cleaned_name = self._clean_name(name)
-        name = self._normalize_name(cleaned_name)
-        parameters = self.object_parameters.copy()
-        _type, encoding = mimetypes.guess_type(name)
-        content_type = getattr(content, "content_type", None)
-        content_type = content_type or _type or self.default_content_type
-        if type(content_type) == bytes:
-            content_type = content_type.decode("utf8")
-
-        # setting the content_type in the key object is not enough.
-        parameters.update({"ContentType": content_type})
-
-        if self.gzip and content_type in self.gzip_content_types:
-            content = self._compress_content(content)
-            parameters.update({"ContentEncoding": "gzip"})
-        elif encoding:
-            parameters.update({"ContentEncoding": encoding})
-
-        encoded_name = self._encode_name(name)
-        obj = self.bucket.Object(encoded_name)
-        if self.preload_metadata:
-            self._entries[encoded_name] = obj
-
-        if isinstance(content, File):
-            content = content.file
-
-        self._save_content(obj, content, parameters=parameters)
-        return cleaned_name
-
-    def _save_content(self, obj, content, parameters):
-        """
-        We create a clone of the content file as when this is passed to boto3
-        it wrongly closes the file upon upload where as the storage backend
-        expects it to still be open
-        """
-        # Seek our content back to the start
-        content.seek(0, os.SEEK_SET)
-
-        # Create a temporary file that will write to disk after a specified
-        # size
-        content_autoclose = SpooledTemporaryFile()
-
-        # Write our original content into our copy that will be closed by boto3
-        content_autoclose.write(content.read())
-        # Upload the object which will auto close the content_autoclose
-        # instance
-        super()._save_content(obj, content_autoclose, parameters)
-
-        # Cleanup if this is fixed upstream our duplicate should always close
-        if not content_autoclose.closed:
-            content_autoclose.close()
+    # def _save(self, name, content):
+    #     cleaned_name = self._clean_name(name)
+    #     name = self._normalize_name(cleaned_name)
+    #     parameters = self.object_parameters.copy()
+    #     _type, encoding = mimetypes.guess_type(name)
+    #     content_type = getattr(content, "content_type", None)
+    #     content_type = content_type or _type or self.default_content_type
+    #     if type(content_type) == bytes:
+    #         content_type = content_type.decode("utf8")
+    #
+    #     # setting the content_type in the key object is not enough.
+    #     parameters.update({"ContentType": content_type})
+    #
+    #     if self.gzip and content_type in self.gzip_content_types:
+    #         content = self._compress_content(content)
+    #         parameters.update({"ContentEncoding": "gzip"})
+    #     elif encoding:
+    #         parameters.update({"ContentEncoding": encoding})
+    #
+    #     encoded_name = self._encode_name(name)
+    #     obj = self.bucket.Object(encoded_name)
+    #     if self.preload_metadata:
+    #         self._entries[encoded_name] = obj
+    #
+    #     if isinstance(content, File):
+    #         content = content.file
+    #
+    #     self._save_content(obj, content, parameters=parameters)
+    #     return cleaned_name
+    #
+    # def _save_content(self, obj, content, parameters):
+    #     """
+    #     We create a clone of the content file as when this is passed to boto3
+    #     it wrongly closes the file upon upload where as the storage backend
+    #     expects it to still be open
+    #     """
+    #     # Seek our content back to the start
+    #     content.seek(0, os.SEEK_SET)
+    #
+    #     # Create a temporary file that will write to disk after a specified
+    #     # size
+    #     content_autoclose = SpooledTemporaryFile()
+    #
+    #     # Write our original content into our copy that will be closed by boto3
+    #     content_autoclose.write(content.read())
+    #     # Upload the object which will auto close the content_autoclose
+    #     # instance
+    #     super()._save_content(obj, content_autoclose, parameters)
+    #
+    #     # Cleanup if this is fixed upstream our duplicate should always close
+    #     if not content_autoclose.closed:
+    #         content_autoclose.close()
+    pass
 
 
 class MediaStorage(PatchedS3Boto3Storage):
