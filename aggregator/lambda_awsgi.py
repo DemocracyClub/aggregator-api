65a07243e14c7491bb1188b585b5a91b713057c5
Sym Roe <sym.roe@talusdesign.co.uk>
2020-12-09 19:49:25 +0000

    Hack to get postcodes with spaces working in prod

--- aggregator/lambda_awsgi.py
+++ aggregator/lambda_awsgi.py
@@ -6,6 +6,9 @@ application = get_wsgi_application()
 
 
 def lambda_handler(event, context):
+    # FIXME: Remove this and figure out how to sort at the Lambda / API gateway level
+    event["path"] = event.get("path").replace("%20", " ")
+
     return awsgi.response(
         application,
         event,
