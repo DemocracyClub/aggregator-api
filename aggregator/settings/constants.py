4d0779255d2f0b50785c06497f01fb7a263cfb82
Sym Roe <sym.roe@talusdesign.co.uk>
2021-04-15 11:23:11 +0100

    Use HTTP for WCIVF API

--- aggregator/settings/constants.py
+++ aggregator/settings/constants.py
@@ -2,6 +2,6 @@ import os
 
 WDIV_BASE_URL = "https://wheredoivote.co.uk/api/beta/"
 WDIV_API_KEY = os.environ.get("WDIV_API_KEY", None)
-WCIVF_BASE_URL = "https://whocanivotefor.co.uk/api/"
+WCIVF_BASE_URL = "http://whocanivotefor.co.uk/api/"
 EE_BASE_URL = "https://elections.democracyclub.org.uk/api/"
 USER_AGENT = "devs.DC API"
