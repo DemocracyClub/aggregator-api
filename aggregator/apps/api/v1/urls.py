d6b8073a31e3c1b6e11d8472780cbd7e835224c7
Sym Roe <sym.roe@talusdesign.co.uk>
2022-02-15 12:37:28 +0000

    Bump versions for python 3.8

--- aggregator/apps/api/v1/urls.py
+++ aggregator/apps/api/v1/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path, path
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView, TemplateView
 from .views import (
@@ -13,38 +13,36 @@ docs_redirect = RedirectView.as_view(url=reverse_lazy("api:v1:docs"))
 
 urlpatterns = [
     # API endpoints
-    url(
+    re_path(
         r"^postcode/(?P<postcode>[A-Za-z0-9 +]+)/$",
         PostcodeView.as_view(),
         name="postcode",
     ),
-    url(r"^address/(?P<slug>[-\w]+)/$", AddressView.as_view(), name="address"),
-    url(
+    re_path(r"^address/(?P<slug>[-\w]+)/$", AddressView.as_view(), name="address"),
+    re_path(
         r"^elections/(?P<slug>[\w.-]+)/$",
         SingleElectionView.as_view(),
         name="elections_get",
     ),
-    url(r"^elections/$", ElectionListView.as_view(), name="elections_list"),
+    path("elections/", ElectionListView.as_view(), name="elections_list"),
     # sandbox endpoints
-    url(
+    re_path(
         r"^sandbox/postcode/(?P<postcode>[A-Za-z0-9 +]+)/$",
         SandboxView.as_view(),
         name="sandbox-postcode",
     ),
-    url(
+    re_path(
         r"^sandbox/address/(?P<slug>[-\w]+)/$",
         SandboxView.as_view(),
         name="sandbox-address",
     ),
     # docs root
-    url(
-        r"^$", TemplateView.as_view(template_name="api_docs_rendered.html"), name="docs"
-    ),
+    path("", TemplateView.as_view(template_name="api_docs_rendered.html"), name="docs"),
     # redirect these to the docs instead of throwing a 404
-    url(r"^postcode/$", docs_redirect),
-    url(r"^address/$", docs_redirect),
-    url(r"^sandbox/postcode/$", docs_redirect),
-    url(r"^sandbox/address/$", docs_redirect),
-    url(r"^sandbox/elections/$", docs_redirect),
-    url(r"^sandbox/$", docs_redirect),
+    path("postcode/", docs_redirect),
+    path("address/", docs_redirect),
+    path("sandbox/postcode/", docs_redirect),
+    path("sandbox/address/", docs_redirect),
+    path("sandbox/elections/", docs_redirect),
+    path("sandbox/", docs_redirect),
 ]
