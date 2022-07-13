b8878e9998c5293c7b2d49d2a6d85ca7d218ee99
Sym Roe <sym.roe@talusdesign.co.uk>
2022-07-13 10:04:18 +0100

    Turn Common lib in to a layer

---
+++ api/common/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="dc_api_common",
+    version="0.0.1",
+    description="Common elements to the DC API",
+    author="Sym Roe",
+    author_email="sym.roe@democracyclub.org.uk",
+    setup_requires=["wheel"],
+    packages=["common"],
+    package_dir={"common": "."},
+    install_requires=[
+        "httpx==0.23.0",
+        "mangum==0.15.0",
+        "starlette==0.19.1",
+    ],
+)
