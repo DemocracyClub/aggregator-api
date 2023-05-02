35f4f7aec0f3d1d8d68adacaf44ae87397588926
GeoWill <roper.william@gmail.com>
2023-05-02 16:14:23 +0100

    Use HTTP/2

--- api/common/http_client.py
+++ api/common/http_client.py
@@ -1,3 +1,3 @@
 import httpx
 
-app_httpx_client = httpx.Client()
+app_httpx_client = httpx.Client(http2=True)
--- api/common/setup.py
+++ api/common/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="dc_api_common",
-    version="0.0.1",
+    version="0.0.2",
     description="Common elements to the DC API",
     author="Sym Roe",
     author_email="sym.roe@democracyclub.org.uk",
@@ -12,7 +12,7 @@ setup(
     packages=["common"],
     package_dir={"common": "."},
     install_requires=[
-        "httpx==0.23.0",
+        "httpx[http2]==0.23.0",
         "mangum==0.17.0",
         "starlette==0.26.1",
         "dc_logging_utils@https://github.com/DemocracyClub/dc_logging/archive/refs/tags/0.0.10.tar.gz",
