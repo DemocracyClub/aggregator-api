62b59c770e0ecf00b6d2c6c32ae0a5c278767ca8
GeoWill <roper.william@gmail.com>
2025-09-09 16:34:49 +0100

    Add ByElectionReason to api

--- api/endpoints/v1/voting_information/stitcher.py
+++ api/endpoints/v1/voting_information/stitcher.py
@@ -363,7 +363,9 @@ class Stitcher:
                 ballot["postal_voting_requirements"] = wcivf_ballot.get(
                     "postal_voting_requirements", None
                 )
-
+                ballot["by_election_reason"] = wcivf_ballot.get(
+                    "by_election_reason", ""
+                )
                 # We only sort by organisation_type at the moment, but we
                 # could add more values here to sort by more fields
                 self.ballot_sort_keys[ballot["ballot_paper_id"]] = wcivf_ballot[
